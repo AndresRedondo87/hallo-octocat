af52c0b94b0db42eb932ac1156cfd7ec3b8e89ee
Jan Suchotzki <jan@lernmoment.de>
2016-05-02 16:29:52 +0200

    Passt MainWindow auf LM-Design an

--- HalloOctocat/MainWindow.xaml.cs
+++ HalloOctocat/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace HalloOctocat
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
