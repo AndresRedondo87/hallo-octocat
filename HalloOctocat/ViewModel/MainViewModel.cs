10f579cec6b98510c805ded138327414081b216c
Jan Suchotzki <jan@lernmoment.de>
2016-05-09 13:52:43 +0200

    Fügt ersten Draft des MainViewModels ein

---
+++ HalloOctocat/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using HalloOctocat.ViewModel.MvvmFoundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HalloOctocat.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+
+        /// <summary>
+        /// Starts showing different Octocats from Octodex. Intervall is 3 seconds.
+        /// </summary>
+        public ICommand StartOctoShow
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+
+    }
+}
